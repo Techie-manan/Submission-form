7c7ac9d2ee5c6e921ea9bd7416c1bb4b25b653f4
Manan <aggarwalmanan18@gmail.com>
2025-12-09 21:56:30 +0530

    Updated

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import './globals.css'
 
 export const metadata = {
-  title: '0xGenIgnite Submission Form',
-  description: 'Participants submit their projects for 0xGenIgnite here',
+  title: 'PPT Submission Form',
+  description: 'Participants submit their projects for Duality Hackathon here',
   icons: {
     icon: '/logo.png', // 👈 Path to your logo file in /public
   },
